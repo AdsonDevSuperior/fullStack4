ad10e6f1621dfa99e1382112c0654421e8b7d215
Silva <adsonplaygg@gmail.com>
2021-09-07 13:32:41 -0300

    Basic Statics Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import NavBar from "components/NavBar";
+import Footer from "components/NavBar/Footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
+
+    // A função só pode ter um retorno, mas com a tag '<> </> pode-se adicionar outros'
+    <>
+    <NavBar />
+    <div className="container">
         <h1 className="text-primary">Hello world</h1>
+        <DataTable />       
     </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/NavBar/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/AdsonDevSuperior/fullStack4.git" target="_blank" rel="noreferrer">Adson Desenvolvedor</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/adson-albino-silva-4306741b9/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
